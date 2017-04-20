c2042dec0080e4a686498d9e64f873c9fb86645f
Oleg Agapov <oagapov@yamoney.ru>
2017-04-20 15:42:14 +0300

    Разбиваем на более мелкие и читаемые ф-ии

--- test/index.js
+++ test/index.js
@@ -9,47 +9,124 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('rangeArrayToString', () => {
-	it('test', () => {
-		return expect(rangeArrayToString([1, 2, 3, 4, 5, 6, 7, 8])).to.eventually.equal('1-8');
+	it('test', (done) => {
+		rangeArrayToString([1, 2, 3, 4, 5, 6, 7, 8])
+			.then((str) => {
+				expect(str).to.equal('1-8');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 1', () => {
-		return expect(rangeArrayToString([1, 3, 4, 5, 6, 7, 8])).to.eventually.equal('1,3-8');
+	it('test 1', (done) => {
+		rangeArrayToString([1, 3, 4, 5, 6, 7, 8])
+			.then((str) => {
+				expect(str).to.equal('1,3-8');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 2', () => {
-		return expect(rangeArrayToString([1, 3, 4, 5, 6, 7, 8, 10, 11, 12])).to.eventually.eql('1,3-8,10-12');
+	it('test 2', (done) => {
+		rangeArrayToString([1, 3, 4, 5, 6, 7, 8, 10, 11, 12])
+			.then((str) => {
+				expect(str).to.equal('1,3-8,10-12');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 3', () => {
-		return expect(rangeArrayToString([1, 2, 3])).to.eventually.eql('1-3');
+	it('test 3', (done) => {
+		rangeArrayToString([1, 2, 3])
+			.then((str) => {
+				expect(str).to.equal('1-3');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 4', () => {
-		return expect(rangeArrayToString([1, 2])).to.eventually.eql('1,2');
+	it('test 4', (done) => {
+		rangeArrayToString([1, 2])
+			.then((str) => {
+				expect(str).to.equal('1,2');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 5', () => {
-		return expect(rangeArrayToString([1, 2, 4])).to.eventually.eql('1,2,4');
+	it('test 5', (done) => {
+		rangeArrayToString([1, 2, 4])
+			.then((str) => {
+				expect(str).to.equal('1,2,4');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 6', () => {
-		return expect(rangeArrayToString([1, 2, 4, 5, 6])).to.eventually.eql('1,2,4-6');
+	it('test 6', (done) => {
+		rangeArrayToString([1, 2, 4, 5, 6])
+			.then((str) => {
+				expect(str).to.equal('1,2,4-6');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 7', () => {
-		return expect(rangeArrayToString([1, 2, 3, 7, 8, 9, 15, 17, 19, 20, 21])).to.eventually.eql('1-3,7-9,15,17,19-21');
+	it('test 7', (done) => {
+		rangeArrayToString([1, 2, 3, 7, 8, 9, 15, 17, 19, 20, 21])
+			.then((str) => {
+				expect(str).to.equal('1-3,7-9,15,17,19-21');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 8', () => {
-		return expect(rangeArrayToString([1, 2, 3, 4, 5, 6, 100, 1091, 1999, 2000, 2001, 2002])).to.eventually.eql('1-6,100,1091,1999-2002');
+	it('test 8', (done) => {
+		rangeArrayToString([1, 2, 3, 4, 5, 6, 100, 1091, 1999, 2000, 2001, 2002])
+			.then((str) => {
+				expect(str).to.equal('1-6,100,1091,1999-2002');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 9', () => {
-		return expect(rangeArrayToString([1])).to.eventually.eql('1');
+	it('test 9', (done) => {
+		rangeArrayToString([1])
+			.then((str) => {
+				expect(str).to.equal('1');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 
-	it('test 10', () => {
-		return expect(rangeArrayToString([1, 3, 5, 7, 9, 11])).to.eventually.eql('1,3,5,7,9,11');
+	it('test 10', (done) => {
+		rangeArrayToString([1, 3, 5, 7, 9, 11])
+			.then((str) => {
+				expect(str).to.equal('1,3,5,7,9,11');
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			});
 	});
 });
